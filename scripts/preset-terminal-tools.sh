33ce609566e5d5a779a337d3f884f69a95bc9d51
Sam Wu <me@naizhao.com>
2022-01-16 00:11:25 +0800

    added powerlevel10k added git\php7\golang changed banner

--- scripts/preset-terminal-tools.sh
+++ scripts/preset-terminal-tools.sh
@@ -11,6 +11,9 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/p
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
 
+# Install powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git ./.oh-my-zsh/custom/themes/powerlevel10k
+
 # Get .zshrc dotfile
 cp $GITHUB_WORKSPACE/scripts/.zshrc .
 
